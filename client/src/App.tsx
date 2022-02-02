3c9535e6279486c7a50608045cd5e0c95b42ef5c
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-02-02 09:12:12 +0530

    added: index.tsx and App.tsx comp

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import {memo} from 'react'
+const App = memo(()=>{
+    return(
+        <>
+            <h1>sfdsf</h1>
+        </>
+    )
+})
+export default App
---
+++ client/src/index.tsx
@@ -0,0 +1,5 @@
+import { StrictMode } from "react"
+import { render } from "react-dom"
+import App from "./App"
+
+render(<StrictMode><App/></StrictMode>, document.getElementById('root'))
