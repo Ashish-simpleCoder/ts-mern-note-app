1ede64377ad4eac33b6c0d2ffbf64c19a6171668
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-04-24 10:25:55 +0530

    removing git origin

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ if (MODE === 'prod') {
     (0, mongoose_1.connect)(MONGO || 'mongodb://localhost:27017/your_db').then(() => console.log('db ok')).catch(() => console.log('failed db'));
 }
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'https://ts-mern-note-app.herokuapp.com', 'https://ashish-simplecoder.github.io/man', 'https://ashish-simplecoder.github.io'],
+    origin: ['http://localhost:3000', 'https://ts-mern-note-app.herokuapp.com', 'https://ashish-simplecoder.github.io/man'],
     // origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT']
