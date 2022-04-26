aa86775e2bc9b2f6ce28ed3f693e8ec7c6f47531
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-04-26 17:01:54 +0530

    domain change

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -45,7 +45,7 @@ exports.handleLogin = (0, asyncWrapper_1.default)((req, res, next) => __awaiter(
                 const cookie = (0, genLoginToken_1.default)(user);
                 //also works for local
                 res.cookie(cookie_name, cookie, { maxAge: 200000000, sameSite: 'none', secure: true, path: '/', httpOnly: true,
-                    domain: process.env.MODE == 'prod' ? 'herokuapp.com' : ''
+                    domain: process.env.MODE == 'prod' ? 'ts-mern-note-app.herokuapp.com' : ''
                 });
                 //also for local
                 // res.cookie(cookie_name,cookie,{maxAge:200000000, sameSite:'none', secure:false, path: '/', httpOnly:true})
